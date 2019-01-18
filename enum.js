78bb61a528596396b214220ea033bf6636e42e36
o-sk <m.ohsuka@gmail.com>
2019-01-18 22:30:56 +0900

    Add enum.ts

---
+++ enum.js
@@ -0,0 +1,7 @@
+var Sports;
+(function (Sports) {
+    Sports[Sports["Soccer"] = 0] = "Soccer";
+    Sports[Sports["BaseBall"] = 1] = "BaseBall";
+    Sports[Sports["Tennis"] = 2] = "Tennis";
+})(Sports || (Sports = {}));
+var s = Sports.BaseBall;
---
+++ enum.ts
@@ -0,0 +1,3 @@
+enum Sports {Soccer, BaseBall, Tennis}
+
+let s: Sports = Sports.BaseBall;
