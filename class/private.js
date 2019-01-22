3ffbb3798a6c8830d1ec729e68dd8d87647821c2
o-sk <m.ohsuka@gmail.com>
2019-01-22 22:41:27 +0900

    Learn private

---
+++ class/private.js
@@ -0,0 +1,6 @@
+var Animal = /** @class */ (function () {
+    function Animal(theName) {
+        this.name = theName;
+    }
+    return Animal;
+}());
---
+++ class/private.ts
@@ -0,0 +1,4 @@
+class Animal {
+  private name: string;
+  constructor(theName: string) { this.name = theName; }
+}
