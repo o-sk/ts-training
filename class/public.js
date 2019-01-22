ca8b9e2c965fc386de91acbce7a030d9ed4aa5cc
o-sk <m.ohsuka@gmail.com>
2019-01-22 22:38:24 +0900

    Learn public

---
+++ class/public.js
@@ -0,0 +1,9 @@
+var Animal = /** @class */ (function () {
+    function Animal(theName) {
+        this.name = theName;
+    }
+    Animal.prototype.move = function (distanceInMeters) {
+        console.log(this.name + " moved " + distanceInMeters + "m.");
+    };
+    return Animal;
+}());
