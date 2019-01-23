f28f63b0bc20efa5e9736b684ed2a953d07355d1
o-sk <m.ohsuka@gmail.com>
2019-01-24 00:38:07 +0900

    Call in another way

--- generics/identity.js
+++ generics/identity.js
@@ -1,5 +1,7 @@
 function identity(arg) {
     return arg;
 }
-var output = identity("myString");
-console.log(output);
+var output1 = identity("myString");
+console.log(output1);
+var output2 = identity("myString");
+console.log(output2);
--- generics/identity.ts
+++ generics/identity.ts
@@ -2,5 +2,8 @@ function identity<T>(arg: T): T {
   return arg;
 }
 
-let output = identity<string>("myString");
-console.log(output);
+let output1 = identity<string>("myString");
+console.log(output1);
+
+let output2 = identity("myString");
+console.log(output2);
